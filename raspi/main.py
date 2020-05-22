2dff16878b6a2760bfae5e3b74eb619893d9ec0f
MD <micheledincecco@yahoo.it>
2020-05-22 23:49:10 +0200

    main clean with correct time

--- raspi/main.py
+++ raspi/main.py
@@ -8,17 +8,17 @@ import serial
 import RPi.GPIO as GPIO
 
 
-#GPIO SET UP
+# GPIO SET UP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
 
 GPIO.output(17,GPIO.LOW)
 
-#pump run time: 30 min
-runtime = 15
+# pump run time: 30 min
+runtime = 30 * 3600
 rest = 10 * 3600
 
-#Serial communication init
+# Serial communication init
 ser = serial.Serial(port='/dev/ttyACM0', 
 baudrate = 9600,
 parity=serial.PARITY_NONE,
@@ -33,11 +33,11 @@ def pump(switch):
     if switch:
         print("Pump start")
         ser.write(str.encode('pon'))
-	GPIO.output(17,GPIO.HIGH)
+        #GPIO.output(17,GPIO.HIGH)
     else:
         print("Pump stop")
         ser.write(str.encode('pof'))
-	GPIO.output(17,GPIO.LOW)
+        #GPIO.output(17,GPIO.LOW)
 
 
 # Main loop
@@ -45,8 +45,8 @@ def main():
 
     while True:
 
-        now_time = datetime.datetime.now().strftime('%H')
-        if (now_time == '17'):
+        now_time = datetime.datetime.now().strftime('%H:%M')
+        if (now_time == '08.00' or now_time == '13.00' or now_time == '18.00'):
             pump(True)
             sleep(runtime)
             pump(False)
