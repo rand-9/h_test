85aab7fd0f0e832f24344866b1bc930889ee0a5a
Michele DIncecco <micheledincecco@yahoo.it>
2020-05-24 22:41:42 +0200

    fix issue merging

--- raspi/main.py
+++ raspi/main.py
@@ -1,38 +1,4 @@
-#!/usr/bin/python
-
-import os
-import sys
-import datetime
-from time import sleep
-import serial
-
-ser = serial.Serial(
-        port='/dev/ttyACM0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
-
-
-#def saveTemp():
-
-
-#def saveHum():
-
-
-def main():
-    while True:
-        #ser.write(str.encode('serialTest \n'))
-        sleep(1)
-        x = ser.readline()
-        print(x)
-        sleep(1)
-
-
-if __name__ == "__main__":
-    main()#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import sys
@@ -53,7 +19,7 @@ runtime = 30 * 3600
 rest = 10 * 3600
 morning = "08:00"
 lunch = "13:00"
-dinner = "20:00"
+dinner = "20:20"
 
 # Serial communication init
 ser = serial.Serial(port='/dev/ttyACM0', 
